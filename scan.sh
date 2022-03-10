78a2d4d099db8bbf3f187848692074d100aa6bcc
menno <menno@lacework.com>
2022-03-10 10:34:06 -0800

    artificacts

--- scan.sh
+++ scan.sh
@@ -3,6 +3,6 @@ IMAGE=$(echo $1 | cut -f1 -d:)
 TAG=$(echo $1 | cut -f2 -d:)
 echo "::set-output name=IMAGE::$IMAGE"
 echo "::set-output name=TAG::$TAG"
-./lw-scanner evaluate $IMAGE $TAG --policy -v=false --html --html-file lw-scan-result.html > lw-scan-results.json
+./lw-scanner evaluate $IMAGE $TAG --policy -v=false --html --html-file lw-scan-result.html > lw-scan-result.json
 ls
 # jq '.' lw-scan-results.json
