204269bb759caa800a4d719678e4f0fe97c387f7
menno <menno@lacework.com>
2022-03-10 09:50:02 -0800

    test

--- scan.sh
+++ scan.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
-which docker
-docker ps
-env
-ls /var/run/*.sock
 IMAGE=$(echo $1 | cut -f1 -d:)
 TAG=$(echo $1 | cut -f2 -d:)
 echo "Scanning:"
 echo "  image=$IMAGE"
 echo "  tag=$TAG"
 ./lw-scanner evaluate $IMAGE $TAG --policy --fail-on-violation-exit-code 1 -v=false > lw-scan-results.json
+jq '.' lw-scan-result.json
