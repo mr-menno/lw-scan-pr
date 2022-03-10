5dfc664b9c1f00d36ac019e874d72a0b503a0c0a
menno <menno@lacework.com>
2022-03-10 09:33:40 -0800

    test

--- scan.sh
+++ scan.sh
@@ -4,5 +4,5 @@ TAG=$(echo $1 | cut -f2 -d:)
 echo "Scanning:"
 echo "  image=$IMAGE"
 echo "  tag=$TAG"
-echo $GITHUB_PATH
+echo $IMAGE_TAG
 ./lw-scanner evaluate $IMAGE $TAG --policy --fail-on-violation-exit-code 1 -v=false > lw-scan-results.json
