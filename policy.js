8bd43f54854f7de8f129b1590700c0038ede9dea
menno <menno@lacework.com>
2022-03-10 14:32:34 -0800

    policy

--- policy.js
+++ policy.js
@@ -28,22 +28,20 @@ policy.result = (github) => {
   });
 
   let message = `
-<h1>Lacework Scanner</h1>
-<p>
-  Scanned image <strong>${results.cve.image.image_info.registry}/${results.cve.image.image_info.repository}:${results.cve.image.image_info.tags.join(',')}</strong><br />
-  Image digest <strong>${results.cve.image.image_info.image_digest}</strong>
-</p>
+# Lacework Scanner
 
-<h2>Vulnerability Summary<h2>
-<table>
-  <tr><th>Severity</th><th>Count</th><th>Fixable</th></tr>
-  <tr><td>Critical</td><td>${vulnCount.critical.found}</td><td>${vulnCount.critical.fixable})</td></tr>
-  <tr><td>High</td><td>${vulnCount.high.found}</td><td>${vulnCount.high.fixable})</td></tr>
-  <tr><td>Medium</td><td>${vulnCount.medium.found}</td><td>${vulnCount.medium.fixable})</td></tr>
-  <tr><td>Low</td><td>${vulnCount.low.found}</td><td>${vulnCount.low.fixable})</td></tr>
-  <tr><td>Info</td><td>${vulnCount.info.found}</td><td>${vulnCount.info.fixable})</td></tr>
-</table>
-  `;
+Scanned image <strong>${results.cve.image.image_info.repository}:${results.cve.image.image_info.tags.join(',')}</strong><br />
+Image digest <strong>${results.cve.image.image_info.image_digest}</strong>
+
+## Vulnerability Summary
+| Severity | Count | Fixable |
+| --- | --- |
+| Critical | ${vulnCount.critical.found} | ${vulnCount.critical.fixable} |
+| High | ${vulnCount.high.found} | ${vulnCount.high.fixable} |
+| Medium | ${vulnCount.medium.found} | ${vulnCount.medium.fixable} |
+| Low | ${vulnCount.low.found} | ${vulnCount.low.fixable} |
+| Info | ${vulnCount.info.found} | ${vulnCount.info.fixable} |
+`;
 
   message += `<h2>Lacework Policies</h2>`;
   if(policies_violated.length>0) {
@@ -60,6 +58,7 @@ policy.result = (github) => {
     message += '<p>all policies have passed</p>'
   }
   console.log(message);
+  console.log(results.cve.image.image_info.image_digest);
   return message;
 }
 
