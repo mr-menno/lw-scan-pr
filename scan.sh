170368d326382f3ae14fff005675fd3a61b13075
menno <menno@lacework.com>
2022-03-10 10:18:40 -0800

    test

--- scan.sh
+++ scan.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 IMAGE=$(echo $1 | cut -f1 -d:)
 TAG=$(echo $1 | cut -f2 -d:)
-echo "Scanning:"
-echo "  image=$IMAGE"
-echo "  tag=$TAG"
+echo "::set-output name=IMAGE::$IMAGE"
+echo "::set-output name=TAG::$TAG"
 ./lw-scanner evaluate $IMAGE $TAG --policy -v=false --html --html-file lw-scan-result.html > lw-scan-results.json
 ls
 # jq '.' lw-scan-results.json
