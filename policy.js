0e23af3ba63c5ff0dbe803f939e3e24b77921a3a
menno <menno@lacework.com>
2022-03-10 14:26:01 -0800

    policy.js

--- policy.js
+++ policy.js
@@ -27,30 +27,37 @@ policy.result = (github) => {
     })
   });
 
-  let message = `Scanned image *${results.cve.image.image_info.registry}/${results.cve.image.image_info.repository}:${results.cve.image.image_info.tags.join(',')}
-  image digest: ${results.cve.image.image_info.image_digest}
-  ---
-  Vulnerability Summary:
-  Critical(fixable): ${vulnCount.critical.found}(${vulnCount.critical.fixable})
-  High(fixable):     ${vulnCount.high.found}(${vulnCount.high.fixable})
-  Medium(fixable):   ${vulnCount.medium.found}(${vulnCount.medium.fixable})
-  Low(fixable):      ${vulnCount.low.found}(${vulnCount.low.fixable})
-  Info(fixable):     ${vulnCount.info.found}(${vulnCount.info.fixable})
-  --
+  let message = `
+<h1>Lacework Scanner</h1>
+<p>
+  Scanned image <strong>${results.cve.image.image_info.registry}/${results.cve.image.image_info.repository}:${results.cve.image.image_info.tags.join(',')}</strong><br />
+  Image digest <strong>${results.cve.image.image_info.image_digest}</strong>
+</p>
+
+<h2>Vulnerability Summary<h2>
+<table>
+  <tr><th>Severity</th><th>Count</th><th>Fixable</th></tr>
+  <tr><td>Critical</td><td>${vulnCount.critical.found}</td><td>${vulnCount.critical.fixable})</td></tr>
+  <tr><td>High</td><td>${vulnCount.high.found}</td><td>${vulnCount.high.fixable})</td></tr>
+  <tr><td>Medium</td><td>${vulnCount.medium.found}</td><td>${vulnCount.medium.fixable})</td></tr>
+  <tr><td>Low</td><td>${vulnCount.low.found}</td><td>${vulnCount.low.fixable})</td></tr>
+  <tr><td>Info</td><td>${vulnCount.info.found}</td><td>${vulnCount.info.fixable})</td></tr>
+</table>
   `;
 
+  message += `<h2>Lacework Policies</h2>`;
   if(policies_violated.length>0) {
     message += `
     <details>
     <summary>Lacework policies have been violated</summary>
     `;
     policies_violated.forEach(p=> {
-      message+=`
-      *${p.Policy.policy_type} - ${p.Policy.policy_name}*<br />
-      ${p.Policy.description}
-      `
+      message+=`<p><strong>${p.Policy.policy_type} - ${p.Policy.policy_name}</strong><br />`;
+      message+=`${p.Policy.description}</p>`;
     })
     message += `</details>`
+  } else {
+    message += '<p>all policies have passed</p>'
   }
   console.log(message);
   return message;
