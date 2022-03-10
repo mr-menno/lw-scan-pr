5ebe3070c1f96877fad2f36513933092ea9be64b
menno <menno@lacework.com>
2022-03-10 11:03:20 -0800

    test

--- scan.sh
+++ scan.sh
@@ -3,7 +3,9 @@ IMAGE=$(echo $1 | cut -f1 -d:)
 TAG=$(echo $1 | cut -f2 -d:)
 echo "::set-output name=IMAGE::$IMAGE"
 echo "::set-output name=TAG::$TAG"
-#./lw-scanner evaluate $IMAGE $TAG --policy -v=false --html --html-file lw-scan-result.html > lw-scan-result.json
-echo '{"test":"two"}' > lw-scan-result.json
+# removed html output while waitnig for bug fix.
+./lw-scanner evaluate $IMAGE $TAG --policy -v=false -q -s > lw-scan-result.json
+#./lw-scanner evaluate $IMAGE $TAG --policy -v=false --html --html-file lw-scan-result.html -q -s > lw-scan-result.json
+# echo '{"test":"two"}' > lw-scan-result.json
 ls
 # jq '.' lw-scan-results.json
