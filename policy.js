5daf181969dd6002978542ded09bfecbff97f655
menno <menno@lacework.com>
2022-03-17 12:46:06 -0700

    walk

--- policy.js
+++ policy.js
@@ -2,12 +2,34 @@ const fs = require('fs');
 
 let policy = {};
 
+var walk = function(dir) {
+  var results = [];
+  var list = fs.readdirSync(dir);
+  list.forEach(function(file) {
+      file = dir + '/' + file;
+      var stat = fs.statSync(file);
+      if (stat && stat.isDirectory()) { 
+          /* Recurse into a subdirectory */
+          results = results.concat(walk(file));
+      } else { 
+          /* Is a file */
+          results.push(file);
+      }
+  });
+  return results;
+}
+
+function findEvaluationJson() {
+  console.log(walk('./lw-scanner-data'));
+}
+
 function santizeTableCell(text) {
   return text.replace('|','')
     .replace("\n","<br />");
 }
 
 policy.result = (github) => {
+  findEvaluationJson();
   let file = fs.readFileSync('lw-scan-results.json');
   let results = JSON.parse(file);
   let policies_violated = results.policy.filter(p=>p.status=='VIOLATED');
